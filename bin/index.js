ec3e8d3e29528331a03d3e088e50cfe8e30c1a86
Jamie Nichols <Jamie337nichols@gmail.com>
2022-02-16 23:56:38 -0500

    Initial full commit

---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const chokidar = require("chokidar")
+const liveServer = require("live-server")
+const { build } = require("esbuild")
+const alias = require("esbuild-plugin-alias")
+const { findJavascriptFiles } = require("../lib/htmlParse");
+const htmlFile = process.argv[2]
+if (!htmlFile) new Error("Must include a Html File")
+const html = fs.readFileSync(htmlFile)
+fs.copyFileSync(htmlFile, `dist/${htmlFile}`)
+const jsFiles = findJavascriptFiles(html)
+      ; (async () => {
+            // `esbuild` bundler for JavaScript / TypeScript.
+            const builder = await build({
+
+                  bundle: true,
+                  loader: { '.js': 'jsx' },
+                  define: { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") },
+                  entryPoints: jsFiles,
+                  incremental: true,
+                  //minify: process.env.NODE_ENV === "production",
+                  //minify: true,
+                  outfile: `dist/index.js`,
+                  plugins: [
+                        alias({
+                              'react': 'reacty',
+                              'reacty': './reacty'
+                        })
+                  ]
+            })
+            chokidar
+                  .watch(["react/**/*.{js,jsx,ts,tsx}", "src/**/*.{js,jsx,ts,tsx}"], {
+                        interval: 0, // No delay
+                  })
+                  .on("all", () => {
+                        builder.rebuild()
+                  })
+            // `liveServer` local server for hot reload.
+            liveServer.start({
+                  // Opens the local server on start.
+                  open: true,
+                  // Uses `PORT=...` or 8080 as a fallback.
+                  port: +process.env.PORT || 8080,
+                  // Uses `public` as the local server folder.
+                  root: "./dist/",
+            })
+      })()
+
+
+
+
+
+
+
