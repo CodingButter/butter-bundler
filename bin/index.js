794f247ba83baa1d21c2616ec21c7a9d150b15cd
Jamie Nichols <Jamie337nichols@gmail.com>
2022-02-18 23:20:49 -0500

    Implamented SSR kinda

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,21 @@
+#!/usr/bin/env node
 const fs = require("fs");
 const chokidar = require("chokidar")
 const liveServer = require("live-server")
-const { build } = require("esbuild")
+const { buildSync, build } = require("esbuild")
 const alias = require("esbuild-plugin-alias")
+const { JSDOM } = require('jsdom');
+const myFetch = require('node-fetch')
 const { findJavascriptFiles } = require("../lib/htmlParse");
-const htmlFile = process.argv[2].indexOf(".js") == -1 ? process.argv[2] : process.arv[3]
+const { exit } = require("process");
+const htmlFile = process.argv[2]
 if (!htmlFile) new Error("Must include a Html File")
 const html = fs.readFileSync(htmlFile)
-fs.copyFileSync(htmlFile, `dist/${htmlFile}`)
+process.env.NODE_ENV !== 'test_build' && fs.copyFileSync(htmlFile, `dist/${htmlFile}`)
+
 const jsFiles = findJavascriptFiles(html)
-const runBuilder = () => {
-      const builder = build({
+const runBuilder = async () => {
+      const builder = await build({
             bundle: true,
             loader: { '.js': 'jsx' },
             define: { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") },
@@ -30,29 +35,93 @@ const runBuilder = () => {
       })
       return builder
 }
-const developeServer = async () => {
-      // `esbuild` bundler for JavaScript / TypeScript.
+const buildSynced = async () => {
+      const result = buildSync({
+            bundle: true,
+            loader: { '.js': 'jsx' },
+            define: { "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development") },
+            entryPoints: jsFiles,
+            minify: false,
+            outdir: `dist`,
+            write: false,
+      })
+      return new TextDecoder("utf-8").decode(result.outputFiles[0].contents);
+}
+
+const startLiveServer = async (open = true) => {
+      liveServer.start({
+            open,
+            port: +process.env.PORT || 80,
+            root: "./dist/",
+      })
+}
+
+const developeServer = async (open = true) => {
       const builder = await runBuilder()
       chokidar
             .watch("src/**/*.{js,jsx,ts,tsx}", {
-                  interval: 0, // No delay
+                  interval: 0,
             })
             .on("all", () => {
                   builder.rebuild()
             })
-      // `liveServer` local server for hot reload.
-      liveServer.start({
-            // Opens the local server on start.
-            open: true,
-            // Uses `PORT=...` or 8080 as a fallback.
-            port: +process.env.PORT || 8080,
-            // Uses `public` as the local server folder.
-            root: "./dist/",
-      })
+      startLiveServer(open);
+      return;
 }
-if (process.env.NODE_ENV !== "production") developeServer()
-else runBuilder()
 
+const developeStatic = async () => {
+      var fetches = 0
+      var errors = []
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+      const serverFetch = async (...args) => {
+            fetches++;
+            var response = { json: () => { } }
+            try {
+                  response = await myFetch(...args)
+                  fetches--
+            } catch (err) {
+                  fetches--
+                  errors.push({ err, args });
+            }
+            return response
+
+      }
+      const javascript = await buildSynced()
+      JSDOM.fromFile(htmlFile, {
+            resources: "usable", pretendToBeVisual: true
+      }).then((dom) => {
+            const { document } = dom.window
+            dom.window.addEventListener('load', function () {
+                  const window = undefined;
+                  eval(javascript)
+                  setInterval(() => {
+                        if (fetches == 0) {
+                              if (errors.length > 0)
+                                    console.log(errors)
+                              fs.writeFileSync("dist/index.html", document.querySelector("html").outerHTML)
+                              fs.writeFileSync("dist/index.js", javascript)
+                              exit("exiting")
+                        }
+                  }, 100);
+            })
+      }).catch((error) => console.log(error))
+}
+
+switch (process.env.NODE_ENV) {
+      case "development":
+            developeServer()
+            break;
+      case "no-static":
+            runBuilder()
+            break;
+      case "static":
+            developeStatic()
+            break;
+      case "test_build":
+            startLiveServer()
+            break;
+      default:
+}
 
 
 
